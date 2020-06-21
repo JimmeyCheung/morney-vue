f98cfa9fa2a4cdfead7a0bb9065d370f6d2d76b1
jimmeyCheung <100178981@qq.com>
2020-06-21 23:13:05 +0800

    组件优化

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -6,23 +6,19 @@
     <NumberPad />
   </Layout>
 </template>
-
-<script lang="js">
+<script lang='ts'>
 import Vue from "vue";
+import { Component } from "vue-property-decorator";
 import Tags from "@/components/Money/Tags.vue";
 import Notes from "@/components/Money/Notes.vue";
 import Types from "@/components/Money/Types.vue";
 import NumberPad from "@/components/Money/NumberPad.vue";
-
-export default Vue.extend({
-  name: "Money",
-  components: { Tags, Notes, Types, NumberPad },
-  data: ()=>{
-    return {
-      tagList: ['衣','食','住','行']
-    }
-  }
-});
+@Component({
+  components: { Tags, Notes, Types, NumberPad }
+})
+export default class extends Vue {
+  tagList: string[] = ["衣", "食", "住", "行"];
+}
 </script>
 
 <style lang="scss">
